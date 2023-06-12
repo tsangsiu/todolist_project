a4565b8a751d5fa883067d208c43c4b816b83189
tsangsiu <tsangsiu@gmail.com>
2023-06-12 21:12:28 +0800

    Add the Gemfile file

--- lib/todolist_project.rb
+++ lib/todolist_project.rb
@@ -1,3 +1,5 @@
+require 'bundler/setup'
+
 # This class represents a todo item and its associated
 # data: name and description. There's also a "done"
 # flag to show whether this todo item is done.
